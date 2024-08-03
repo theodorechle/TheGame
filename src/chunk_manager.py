938fa90fea13a7adf842de4d3c24bc23dc9d84cc
Théodore <theodore.chailloleauleclerc@gmail.com>
2024-08-03 11:31:59 +0200

    Optimized a lot the display, especially with lots of zones not loaded who where previously displayed

--- src/chunk_manager.py
+++ src/chunk_manager.py
@@ -96,30 +96,24 @@ class ChunkManager:
     
     def display_chunks(self, x: int, y: int) -> None:
         window_size = self.window.get_size()
-        nb_blocks_length = ceil(window_size[0] / blocks.Block.BLOCK_SIZE) + 2
-        nb_blocks_height = ceil(window_size[1] / blocks.Block.BLOCK_SIZE) + 2
-        for i in range(x - nb_blocks_length // 2, x + nb_blocks_length // 2):
-            for j in range(y - nb_blocks_height // 2,  y + nb_blocks_height // 2):
+        nb_blocks_length = ceil(window_size[0] / blocks.Block.BLOCK_SIZE + 2)
+        nb_blocks_height = ceil(window_size[1] / blocks.Block.BLOCK_SIZE + 2)
+        # display the minimum between the window size and the number of loaded blocks
+        min_x = max(x - nb_blocks_length // 2, (self.chunk_x_position - self.nb_chunks_by_side) * Chunk.LENGTH - Chunk.LENGTH // 2)
+        max_x = min(x + nb_blocks_length // 2, (self.chunk_x_position + self.nb_chunks_by_side) * Chunk.LENGTH + Chunk.LENGTH // 2)
+        min_y = max(0, y - nb_blocks_height // 2)
+        max_y = min(Chunk.HEIGHT, y + nb_blocks_height // 2)
+        for i in range(min_x, max_x):
+            for j in range(min_y, max_y):
                 self.display_block(i, j, -x, -y)
     
     def display_block(self, x: int, y: int, x_add: int, y_add: int) -> None:
-        if y < 0 or y >= Chunk.HEIGHT: return
-        x += Chunk.LENGTH // 2
-        block_x = x - self.chunk_x_position * Chunk.LENGTH
-        nb_chunk: int = block_x // Chunk.LENGTH + self.nb_chunks_by_side
-        if nb_chunk < 0 or nb_chunk >= len(self.chunks): return # out of loaded chunks
-        chunk = self.chunks[nb_chunk]
-        block = chunk.blocks[y][block_x % Chunk.LENGTH]
-        block_image = block.image
+        block_x = x + Chunk.LENGTH // 2 - self.chunk_x_position * Chunk.LENGTH
+        chunk = self.chunks[block_x // Chunk.LENGTH + self.nb_chunks_by_side]
         window_size = self.window.get_size()
-        coords = window_size[0] // 2 + blocks.Block.BLOCK_SIZE*(x - Chunk.LENGTH // 2 + x_add - 0.5), window_size[1] // 2 - blocks.Block.BLOCK_SIZE*(y + 1 + y_add)
-        self.window.blit(
-            blocks.AIR.image,
-            coords
-        )
-        self.window.blit(
-            block_image,
-            coords
+        coords = window_size[0] // 2 + blocks.Block.BLOCK_SIZE*(x + x_add - 0.5), window_size[1] // 2 - blocks.Block.BLOCK_SIZE*(y + 1 + y_add)
+        self.window.blits(
+            ((blocks.AIR.image, coords), (chunk.blocks[y][block_x % Chunk.LENGTH].image, coords))
         )
 
     def save(self) -> None:
