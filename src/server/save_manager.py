76104d76c4cddb6a540e25732db0dcbdd7fa236d
Théodore <theodore.chailloleauleclerc@gmail.com>
2024-10-19 18:15:33 +0200

    Fix 2

--- src/server/save_manager.py
+++ src/server/save_manager.py
@@ -14,6 +14,7 @@ CURRENT_VERSION = 0.6
 class SaveManager(SaveManagerInterface):
     def __init__(self, save_name: str) -> None:
         self.save_name = save_name
+        os.makedirs(SAVES_PATH, exist_ok=True)
         self.save_path = os.path.join(SAVES_PATH, self.save_name)
         self.init_repository()
 
